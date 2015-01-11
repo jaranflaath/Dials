c4936a45b7332dfd1dbac1f18d85e0eab2f1df5c
Jaran Nilsen <jaran.nilsen@gmail.com>
2015-01-11 21:07:12 +0100

    Begin adding text to dials

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 static Window *s_main_window;
 static Layer *s_canvas;
 static TimeUnits lowest_dial_unit = MINUTE_UNIT;
+static GFont intervals_font = FONT_KEY_GOTHIC_14;
 
 static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(this_layer);
@@ -13,9 +14,16 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   GPoint dial3_center = GPoint(bounds.size.w / 4, bounds.size.h / 4 * 3);
   GPoint dial4_center = GPoint(bounds.size.w / 4 * 3, bounds.size.h / 4 * 3);
 
+  GRect dial1_interval_top_rect = GRect(dial1_center.x, dial1_center.y - 15, 30, 14);
+
   // Draw the 'loop' of the 'P'
   graphics_context_set_stroke_color(ctx, GColorWhite);
   graphics_draw_circle(ctx, dial1_center, 30);
+
+  //graphics_draw_text(ctx, "60\0", intervals_font, dial1_interval_top_rect,
+  //GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
+
   graphics_draw_circle(ctx, dial2_center, 30);
   graphics_draw_circle(ctx, dial3_center, 30);
   graphics_draw_circle(ctx, dial4_center, 30);
