8b286779dd03a4c05562aefa0fedb802646b13fd
Jaran Nilsen <jaran.nilsen@gmail.com>
2015-01-29 21:37:30 +0100

    Add dial numbers

--- src/main.c
+++ src/main.c
@@ -3,9 +3,15 @@
 static Window *s_main_window;
 static Layer *s_canvas;
 static TimeUnits lowest_dial_unit = MINUTE_UNIT;
-static GFont intervals_font = FONT_KEY_GOTHIC_14;
+static GFont intervals_font;
+static GColor background_color = GColorWhite;
+static GColor foreground_color = GColorBlack;
 
 static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+
+  graphics_context_set_stroke_color(ctx, foreground_color);
+  graphics_context_set_text_color(ctx, foreground_color);
+
   GRect bounds = layer_get_bounds(this_layer);
 
   GPoint center = GPoint(bounds.size.w / 2, (bounds.size.h / 2));
@@ -14,16 +20,16 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   GPoint dial3_center = GPoint(bounds.size.w / 4, bounds.size.h / 4 * 3);
   GPoint dial4_center = GPoint(bounds.size.w / 4 * 3, bounds.size.h / 4 * 3);
 
-  GRect dial1_interval_top_rect = GRect(dial1_center.x, dial1_center.y - 15, 30, 14);
+  GRect dial1_interval_top_rect = GRect(dial1_center.x - 15, dial1_center.y - 30, 30, 14);
+  GRect dial1_interval_bottom_rect = GRect(dial1_center.x - 15, dial1_center.y + 10, 30, 14);
 
-  // Draw the 'loop' of the 'P'
-  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_text(ctx, "60", intervals_font, dial1_interval_top_rect,
+    GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
+  graphics_draw_text(ctx, "30", intervals_font, dial1_interval_bottom_rect,
+    GTextOverflowModeFill, GTextAlignmentCenter, NULL);
+
   graphics_draw_circle(ctx, dial1_center, 30);
-
-  //graphics_draw_text(ctx, "60\0", intervals_font, dial1_interval_top_rect,
-  //GTextOverflowModeFill, GTextAlignmentCenter, NULL);
-
-
   graphics_draw_circle(ctx, dial2_center, 30);
   graphics_draw_circle(ctx, dial3_center, 30);
   graphics_draw_circle(ctx, dial4_center, 30);
@@ -32,7 +38,7 @@ static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
 
 static void main_window_load(Window *window) {
 
-  window_set_background_color(s_main_window, GColorBlack);
+  window_set_background_color(s_main_window, background_color);
 
   Layer *window_layer = window_get_root_layer(s_main_window);
   GRect window_bounds = layer_get_bounds(window_layer);
@@ -53,6 +59,9 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void init() {
+
+  intervals_font = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+
   s_main_window = window_create();
 
   window_set_window_handlers(s_main_window, (WindowHandlers){
